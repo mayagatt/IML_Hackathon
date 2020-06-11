7926ec1746c4e7137cc8ddf7d33c90462c14463e
Maya Harari <mayaharari@cs.huji.ac.il>
2020-06-11 22:44:08 +0300

    Add files via upload
    now is pretty

--- eda_process.py
+++ eda_process.py
@@ -1,31 +1,32 @@
+"""
+===================================================
+     Introduction to Machine Learning (67577)
+             IML HACKATHON, June 2020
+
+Authors: Hadas Nahshon, Guy Lutzker, Maya Harari, Omer Plotnik
+
+===================================================
+"""
+
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
-import numpy as np
-from sklearn.naive_bayes import MultinomialNB
-import data_preprocessing as dpre
+import data_preprocessing
 
-train, test = dpre.create_train_test_data()  # create train and test dataframes
-train_text = train['string_group']  # extract train text from train
-train_target = train['project_number']  # extract train_text labels from train
-test_text = test['string_group']  # extract test text from test
-test_target = test['project_number']  # extract test_text labels from test
+# create train and test dataframes
+train, test = data_preprocessing.create_train_test_data()
 
-
-BUILDING_TOOL_PATH = "building_tool_all_data.txt"  # 0
-JINA_PATH = "jina_all_data.txt"  # 1
-HOROVOD_PATH = "horovod_all_data.txt"  # 2
-ESPNET_PATH = "espnet_all_data.txt"  # 3
-PYSOLFC_PATH = "PySolFC_all_data.txt"  # 4
-PADDLEHUB_PATH = "PaddleHub_all_data.txt"  # 5
-PYTORCH_GEOMETRIC_PATH = "pytorch_geometric_all_data.txt"  # 6
-ENCODING = "utf8"
-
-path_lst = [BUILDING_TOOL_PATH, ESPNET_PATH, HOROVOD_PATH, JINA_PATH,
-            PADDLEHUB_PATH, PYSOLFC_PATH, PYTORCH_GEOMETRIC_PATH]
+# split to test, train texts and labels
+train_text, train_target = train['string_group'], train['project_number']
+test_text, test_target = test['string_group'], test['project_number']
 
 
 def fit_model(model):
+    '''
+
+    :param model: to be fitted
+    :return: fitted model
+    '''
     text_clf = Pipeline([('vect', CountVectorizer()),
                          ('tfidf', TfidfTransformer()),
                          ('clf', model)])
@@ -34,7 +35,13 @@ def fit_model(model):
     return text_clf
 
 
-def score_model(clf, docs_new, docs_new_target):
-    score = clf.score(docs_new, docs_new_target)
-    return score
+def score_model(clf, test_data, test_target):
+    '''
 
+    :param clf: modle, to score
+    :param test_data: to predict labels for
+    :param test_target: true labels of test data
+    :return: model score over test data
+    '''
+    score = clf.score(test_data, test_target)
+    return score
