4e931ec92c96a521e50448213ca91de46b1afeb0
Omer Plotnik <omer.plotnik@cs.huji.ac.il>
2020-06-11 23:10:33 +0300

    Add files via upload

--- train_and_save_model.py
+++ train_and_save_model.py
@@ -10,6 +10,7 @@ Authors: Hadas Nahshon, Guy Lutzker, Maya harari, Omer Plotnik
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 import data_preprocessing as dpre
@@ -19,9 +20,24 @@ import eda_process
 
 ##train final model and save it to a pkl file
 
+##parse data and evaluate model score
 train, test = dpre.create_train_test_data()  # create train and test dataframes
-all = pd.concat(train, test, ignore_index=True)
-all_text = train['string_group']  # extract train text from train
-model = eda_process.fit_model(MultinomialNB())
+model = eda_process.fit_model(LinearSVC(C=1.6, random_state=25, tol=1e-5))
+score = eda_process.score_model(model, test['string_group'], test['project_number'])
+print(score)
+
+## train final model and save it in a pkl file
+all_data = pd.concat([train, test], ignore_index=True)
+all_text = all_data['string_group']  # extract train text from train
+all_target = all_data['project_number']
+
+final_model = Pipeline([('vect', CountVectorizer()),
+                         ('tfidf', TfidfTransformer()),
+                         ('clf', LinearSVC(C=1.6, random_state=25, tol=1e-5))])
+
+final_model.fit(all_text, all_target)
+
 joblib_file = "trained_model.pkl"
-joblib.dump(model, joblib_file)
+joblib.dump(final_model, joblib_file)
+
+print("done!")
